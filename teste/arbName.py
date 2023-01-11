522355b16974eb7528d88928af0f4473f2788f00
PedroHGLima <pedrohglima01@gmail.com>
2023-01-10 23:19:54 -0300

    exclusão dos testes

--- teste/arbName.py
+++
@@ -1,8 +0,0 @@
-def someFunction(text):
-    r = 2 + text
-    print ('You passed this Python program ' +str(r)+ ' from C! Congratulations!')
-    return 12345
-
-def outraFuncao(text):
-    print('I am another function ' + text)
-    return 123456
